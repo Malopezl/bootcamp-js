1d9c8000cc4ec94141f2644f906b844f7957432e
Marvin <malopez@takitosu.localdomain>
2022-01-05 01:04:07 -0600

    agregando middleware para guardar estados

--- jsapp_01/src/store/index.js
+++ jsapp_01/src/store/index.js
@@ -1,7 +1,10 @@
 import { applyMiddleware, createStore } from "redux";
 import * as $store from "./store";
 
-const preloadedState = {
+const savedState = localStorage.getItem("state");
+const deserialized = savedState && JSON.parse(savedState);
+
+const preloadedState = deserialized || {
     producto: {},
     productos: []
 };
@@ -10,6 +13,7 @@ const middlewares = applyMiddleware(
     $store.loggerMiddleware,
     $store.agregarOModificarProductoMiddleware,
     $store.generadorCodigoProductoBuilder(0),
+    $store.storageMiddleware,
 );
 
 const store = createStore($store.reducer, preloadedState, middlewares);
