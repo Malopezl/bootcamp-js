5a39b977409466645ad679ede2ddc4448757d011
Marvin <malopez@takitosu.localdomain>
2022-01-06 18:57:29 -0600

    promesas con fetch desde frontend

--- jsapp_01/frontend/src/index.js
+++ jsapp_01/frontend/src/index.js
@@ -11,3 +11,34 @@ ReactDom.render(
         <App />
     </Provider>,
     rootElement);
+
+console.log("Antes del fetch");
+const promesa = fetch('http://localhost:5000/productos');
+
+let productos = null;
+
+/*
+* Una promesa se ejecuta en segundo plano,
+* por lo que el codigo dentro de la promesa no se ejecuta
+* hasta que ya se ejecutaron todas las lineas de codigo
+* que estan fuera de la promesa.
+*/
+promesa
+    // este then lee el body del response y lo pasa al siguiente
+    .then((response) => {
+        return response.json();
+    })
+    // este then recibe el json del then anterior
+    .then((response) => {
+        productos = response;
+        console.log("success: ", productos);
+        return "success";
+    })
+    .then((p) => {
+        console.log('tercer then: ', p);
+    })
+    .catch(() => {
+        console.log("error");
+    });
+
+console.log("Luego del fetch ", productos);
